30b91710668478b1422c8a1eec252583cac3c69e
Jason Simmons <celetavaro@gmail.com>
2017-09-04 03:33:30 -0600

    added rough draft of MakeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -8,6 +8,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    inv <- matrix()
+    set <- function (y){ # setter for matrix
+        
+        x <<- y
+        inv <<- matrix()
+    }
+    get <- function() x # getter for matrix
+    setinv <- function(inverse) inv <<- inverse # setter for inverse matrix
+    getinv <- function() inv #getter for inverse matrix
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
